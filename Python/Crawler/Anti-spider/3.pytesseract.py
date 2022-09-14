9820bea2f1b3bf14596f1d4cc380254df9a6063d
Kidays <shenxianliang@live.cn>
2022-09-14 21:22:50 +0800

    20220914

--- Python/Crawler/Anti-spider/3.pytesseract.py
+++ Python/Crawler/Anti-spider/3.pytesseract.py
@@ -5,11 +5,12 @@ import os
 from PIL import ImageEnhance
 os.chdir(sys.path[0])
 im = Image.open('www.chemsrc.com_casindex_.png')
+# im = Image.open('cas.png')
 im = im.convert('L')  # 'RGB'
 print(im.size)
 # cropped=im.crop((950,410,1899,6500))
 # cropped = im.crop((1250, 410, 1899, 6500))
-im = im.crop((1500, 6350, 1899, 6500))  # L,T,R,B
+# im = im.crop((1500, 6350, 1899, 6500))  # L,T,R,B
 # enhancer = ImageEnhance.Color(im)
 # enhancer = enhancer.enhance(0)
 # enhancer = ImageEnhance.Brightness(enhancer)
