e06449b1cb61e03efbacefc7b69ce02cfd6a77ed
Kidays <shenxianliang@live.cn>
2022-11-05 22:02:58 +0800

    20221105

---
+++ Python/OpenCV-Tutorials/57.Car license.py
@@ -0,0 +1,21 @@
+# from PIL import Image
+from paddleocr import PaddleOCR
+# import sys
+# import os
+# os.chdir(sys.path[0])
+ocr=PaddleOCR(use_angle_cls=True,lang='en')
+path=r'H:\MyGitCodes\Python\Crawler\Anti-spider\plate.png'
+result=ocr.ocr(path,cls=True)
+# print(result)
+for text in result:
+    print(text[1][0])
+# result=ocr.ocr(im)
+# print(result)
+# from PIL import Image
+# import pytesseract
+# import sys
+# import os
+# os.chdir(sys.path[0])
+# im = Image.open('www.chemsrc.com_casindex_.png')
+# im = im.convert('L')  # 'RGB'
+# print(im.size)
